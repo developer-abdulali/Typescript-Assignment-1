879bb2c6fadceebcf0f6b214a95b06ff21a97d59
developer-abdulali <ab.ali.soomro@gmail.com>
2023-07-09 01:56:06 +0500

    added task 1 file

---
+++ task1.ts
@@ -0,0 +1,5 @@
+// Storing person's name in a variable
+var personName = "Eric"
+
+// Printing  message with the person's name
+console.log("Hello " + personName + ", would you like to learn some Typescript today?")
