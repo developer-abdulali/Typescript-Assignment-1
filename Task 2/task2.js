d1c342285c6a835f092c18fab79524b33f7b6988
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-07-09 02:16:41 +0500

    Add files via upload

---
+++ Task 2/task2.js
@@ -0,0 +1,8 @@
+// Storing person name in a variable
+var onePersonName = "Ghulam Murtaza";
+// Printing person name in lowercase
+console.log(onePersonName.toLowerCase());
+// Storing person name in a variable
+var secondPersonName = "Ghulam Murtaza";
+// Printing person name in UpperCase
+console.log(secondPersonName.toUpperCase());
---
+++ Task 2/task2.ts
@@ -0,0 +1,11 @@
+// Storing person name in a variable
+var onePersonName = "Ghulam Murtaza";
+
+// Printing person name in lowercase
+console.log(onePersonName.toLowerCase());
+
+// Storing person name in a variable
+var secondPersonName = "Ghulam Murtaza";
+
+// Printing person name in UpperCase
+console.log(secondPersonName.toUpperCase())
