80a3c5faa8e5fea387415276d9de5c84a856f1d6
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-07-09 02:03:02 +0500

    Delete task1.ts

--- task1.ts
+++
@@ -1,5 +0,0 @@
-// Storing person's name in a variable
-var personName = "Eric"
-
-// Printing  message with the person's name
-console.log("Hello " + personName + ", would you like to learn some Typescript today?")
