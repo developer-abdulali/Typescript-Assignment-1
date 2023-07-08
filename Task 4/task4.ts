236d86a055410761d624e6a6b5848a395539eaf3
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-07-09 03:03:31 +0500

    Add files via upload

---
+++ Task 4/task4.ts
@@ -0,0 +1,8 @@
+// Storing poetry in a variable
+var poetry = "دِل میں خُدا کا ہونا لازم ہے اقبال سجدوں میں پڑے رہنے سے جنّت نہیں مِلتی";
+
+// Storing poet name in a variable
+let poet = "Allama Iqbal";
+
+// Print poet name with poetry
+console.log(`${poet} said, ${poetry}`);
