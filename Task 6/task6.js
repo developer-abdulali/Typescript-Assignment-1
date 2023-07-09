fb7e7c29e54cd0df26e48eec5197bef4e015dc64
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-07-09 16:42:38 +0500

    Add files via upload

---
+++ Task 6/task6.js
@@ -0,0 +1,8 @@
+// 1st printing with addition 4 + 4 = 8
+console.log("Addition ".concat(4 + 4)); // 8
+// 2nd printing with subtraction 16 - 8 = 8
+console.log("Subtraction ".concat(16 - 8)); // 8
+// 3rd printing with multiplication 4 x 2 = 8
+console.log("Multiplication ".concat(4 * 2)); // 8
+// 4th printing with division  32 / 4 = 8
+console.log("Division ".concat(32 / 4)); // 8
---
+++ Task 6/task6.ts
@@ -0,0 +1,11 @@
+// 1st printing with addition 4 + 4 = 8
+console.log(`Addition ${4+4}`);  // 8
+
+// 2nd printing with subtraction 16 - 8 = 8
+console.log(`Subtraction ${16-8}`);  // 8
+
+// 3rd printing with multiplication 4 x 2 = 8
+console.log(`Multiplication ${4*2}`);  // 8
+
+// 4th printing with division  32 / 4 = 8
+console.log(`Division ${32/4}`);  // 8
