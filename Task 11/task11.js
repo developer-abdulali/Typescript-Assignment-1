1492dd777931ba146a8f5bd94fc533737e198ce9
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-07-10 01:56:14 +0500

    Add files via upload

---
+++ Task 11/task11.js
@@ -0,0 +1,7 @@
+// Store examples of modes of transportation in an array
+var transportationModes = ["car", "motorcycle", "bicycle", "train", "plane"];
+// Print statements about each mode of transportation
+for (var i = 0; i < transportationModes.length; i++) {
+    var mode = transportationModes[i];
+    console.log("I would like to own a ".concat(mode, "."));
+}
---
+++ Task 11/task11.ts
@@ -0,0 +1,8 @@
+// Store examples of modes of transportation in an array
+const transportationModes = ["car", "motorcycle", "bicycle", "train", "plane"];
+
+// Print statements about each mode of transportation
+for (let i = 0; i < transportationModes.length; i++) {
+    const mode = transportationModes[i];
+    console.log(`I would like to own a ${mode}.`);
+}
