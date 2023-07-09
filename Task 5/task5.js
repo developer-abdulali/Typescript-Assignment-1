3624d4e530aa6080d015b31a3e291decc94aef80
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-07-09 15:53:32 +0500

    Add files via upload

---
+++ Task 5/task5.js
@@ -0,0 +1,6 @@
+var person = "\t John Doe \n";
+console.log("".concat(person));
+console.log(person);
+var strippedName = person.trim();
+console.log("".concat(person));
+console.log(strippedName);
---
+++ Task 5/task5.ts
@@ -0,0 +1,7 @@
+let person: string = "\t John Doe \n";
+console.log(`${person}`);
+console.log(person);
+
+let strippedName: string = person.trim();
+console.log(`${person}`);
+console.log(strippedName);
