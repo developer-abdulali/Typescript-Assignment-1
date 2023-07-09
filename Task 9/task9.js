25f80748a26c80d9b8792a1dc19c31b4ffbdcf97
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-07-10 01:06:44 +0500

    Add files via upload

---
+++ Task 9/task9.js
@@ -0,0 +1,6 @@
+// Storing friends Names in a array
+var friendsNames = ["Usama", "Abdul Ghaffoor", "Shahbaz Ahmed", "Ghulam Rasool"];
+// Using for loop for printing each friends Names
+for (var i = 0; i < friendsNames.length; i++) {
+    console.log(friendsNames[i]);
+}
---
+++ Task 9/task9.ts
@@ -0,0 +1,7 @@
+// Storing friends Names in a array
+var friendsNames: string[] = ["Usama","Abdul Ghaffoor","Shahbaz Ahmed","Ghulam Rasool"];
+
+// Using for loop for printing each friends Names
+for (let i = 0; i < friendsNames.length; i++) {
+    console.log(friendsNames[i])
+}
