bc5b9a5ec9a2d8d619932f4232205402825c82de
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-07-09 16:26:43 +0500

    Delete task6.js

--- Task 6/task6.js
+++
@@ -1,8 +0,0 @@
-// 1st printing with addition 4 + 4 = 8
-console.log(4 + 4); // 8
-// 2nd printing with minus 16 - 8 = 8
-console.log(16 - 8); // 8
-// 3rd printing with substriction 4 x 2 = 8
-console.log(4 * 2); // 8
-// 4th printing with divide  4 x 2 = 8
-console.log(32 / 4); // 8
